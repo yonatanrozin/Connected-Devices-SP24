78c263eb15d15e3cfc580c78a4beba4f3a462ae9
Yonatan Rozin <yonatanrozin42@gmail.com>
2024-03-27 17:39:17 -0400

    Update Arduino_HTTP_Get.ino

--- Week 9/Arduino_HTTP_Get/Arduino_HTTP_Get.ino
+++ Week 9/Arduino_HTTP_Get/Arduino_HTTP_Get.ino
@@ -9,7 +9,7 @@ char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
 char serverAddress[] = "68.183.134.255";  // server IP address
-int port = 1880; //HTTP default is 80, or use a different port
+int port = 1880; //HTTP default is 80, or use 1880 for node-red
 
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, serverAddress, port);
